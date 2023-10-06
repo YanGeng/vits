42bf56a99d8e315dfaaada80b952308a95e50582
YanGeng <18516054923@163.com>
2023-10-06 14:16:44 +0800

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -12,5 +12,14 @@ _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜ
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
 
+'''
+# chinese_cleaners
+_pad        = '_'
+_punctuation = '，。！？—…“”（）'
+_letters = 'ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦㄧㄨㄩˉˊˇˋ˙ '
+
+symbols = [_pad] + list(_punctuation) + list(_letters)
+'''
+
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
